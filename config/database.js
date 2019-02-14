065548cb7df86166c5d5952df06d25599e9c3abb
Paul Wallington <pwallington@gmail.com>
2019-02-14 11:28:52 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // database: 'mongodb://localhost:27017/meanauth',
-  database: 'mongodb://admin:hunter2@ds121980.mlab.com:23685/upslope-db',
+  database: 'mongodb://admin:hunter2@ds121980.mlab.com:21980/upslope-db',
   // secret: 'yoursecret'
 }
